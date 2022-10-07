6002cf711dae95132eed1d6f53748ac3d2783056
Beto Toro <betotoro0902@gmail.com>
2022-10-07 10:45:26 -0500

    [feat]: add and end game logic

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -24,6 +24,15 @@ struct ContentView: View {
   
   func removeCard(at index: Int) {
     cards.remove(at: index)
+    
+    if cards.isEmpty {
+      isActive = false
+    }
+  }
+  func resetCards() {
+    cards = [Card](repeating: Card.example, count: 10)
+    timeRemaining = 100
+    isActive = true
   }
   
   var body: some View {
@@ -48,6 +57,15 @@ struct ContentView: View {
             }
               .stacked(at: index, in: cards.count)
           }
+        }
+        .allowsHitTesting(timeRemaining > 0)
+        
+        if cards.isEmpty {
+          Button("Start Again", action: resetCards)
+            .padding()
+            .background(.white)
+            .foregroundColor(.black)
+            .clipShape(Capsule())
         }
       }
       if differentiateWithoutColor {
@@ -80,7 +98,9 @@ struct ContentView: View {
     }
     .onChange(of: scenePhase) { newPhase in
       if newPhase == .active {
-        isActive = true
+        if cards.isEmpty == false {
+          isActive = true
+        }
       } else {
         isActive = false
       }
