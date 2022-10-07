a003ab774a2c465dd77312e6444d724cb119f4d6
Beto Toro <betotoro0902@gmail.com>
2022-10-07 10:21:29 -0500

    [feat]: add accesibility and a color diferentiation when it's ok or wrong

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -7,14 +7,64 @@
 
 import SwiftUI
 
+extension View {
+  func stacked(at position: Int, in total: Int) -> some View {
+    let offset = Double(total - position)
+    return self.offset(x: 0, y: offset * 10)
+  }
+}
+
 struct ContentView: View {
-    var body: some View {
-      CardView(card: Card.example)
+  @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+  @State private var cards = [Card](repeating: Card.example, count: 10)
+  
+  func removeCard(at index: Int) {
+    cards.remove(at: index)
+  }
+  
+  var body: some View {
+    ZStack {
+      Image("background")
+        .resizable()
+        .ignoresSafeArea()
+      VStack {
+        ZStack {
+          ForEach(0..<cards.count, id: \.self) { index in
+            CardView(card: cards[index]) {
+              withAnimation {
+                removeCard(at: index)
+              }
+            }
+              .stacked(at: index, in: cards.count)
+          }
+        }
+      }
+      if differentiateWithoutColor {
+        VStack {
+          Spacer()
+          
+          HStack {
+            Image(systemName: "xmark.circle")
+              .padding()
+              .background(.black.opacity(0.7))
+              .clipShape(Circle())
+            Spacer()
+            Image(systemName: "checkmark.circle")
+              .padding()
+              .background(.black.opacity(0.7))
+              .clipShape(Circle())
+          }
+          .foregroundColor(.white)
+          .font(.largeTitle)
+          .padding()
+        }
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
--- Flashzilla/views/CardView.swift
+++ Flashzilla/views/CardView.swift
@@ -9,12 +9,26 @@ import SwiftUI
 
 struct CardView: View {
   let card: Card
+  @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
   @State private var isShowingAnswer = false
+  @State private var offset = CGSize.zero
+  var removal: (() -> Void)? = nil
   
   var body: some View {
     ZStack {
       RoundedRectangle(cornerRadius: 25, style: .continuous)
-        .fill(.white)
+        .fill(
+          differentiateWithoutColor
+          ? .white
+          : .white
+            .opacity(1 - Double(abs(offset.width / 50)))
+        )
+        .background(
+          differentiateWithoutColor
+          ? nil
+          : RoundedRectangle(cornerRadius: 25, style: .continuous)
+            .fill(offset.width > 0 ? .green : .red)
+        )
         .shadow(radius: 10)
       
       VStack {
@@ -30,11 +44,24 @@ struct CardView: View {
       }
       .padding(20)
       .multilineTextAlignment(.center)
-      .onTapGesture {
-        isShowingAnswer.toggle()
-      }
     }
     .frame(width: 450, height: 250)
+    .rotationEffect(.degrees(Double(offset.width / 5)))
+    .offset(x: offset.width * 5, y: 0)
+    .opacity(2 - Double(abs(offset.width / 50)))
+    .gesture(
+      DragGesture()
+        .onChanged { gesture in
+          offset = gesture.translation
+        }
+        .onEnded { _ in
+          if abs(offset.width) > 100 {
+            removal?()
+          } else {
+            offset = .zero
+          }
+        }
+    )
   }
 }
 struct CardView_Previews: PreviewProvider {
